7490062596fcc81b2f0e195a9549355e779b89de
aisaksson187-create <aisaksson187@gmail.com>
2026-04-17 16:22:26 +0200

    Added scripts to start server from rootfolder

--- Client/src/pages/AboutPage.jsx
+++ Client/src/pages/AboutPage.jsx
@@ -4,27 +4,26 @@ export default function AboutPage() {
       <h1>Om projektet</h1>
 
       <p>
-        Det här projektet är ett Wordle-inspirerat spel byggt med React i frontend
-        och Node/Express i backend.
+        Det här projektet är ett Wordle spel som är skapat för fullstack kursen.
       </p>
 
       <p>
         Spelet hämtar ett slumpmässigt ord från servern. Användaren kan välja
-        ordlängd och om ordet får innehålla upprepade bokstäver.
+        ordets längd och om ordet får innehålla upprepade bokstäver.
       </p>
 
       <p>
-        När spelaren gissar ett ord skickas det till backend, där feedback räknas ut.
-        Bokstäver visas sedan som rätt placerade, felplacerade eller felaktiga.
+        När du som spelare gissat ett ord skickas det till backend, där kontrolleras svaret om det är rätt, 
+        fel eller innehåller rätt bokstäver men på fel plats.
       </p>
 
       <p>
-        När spelet är vunnet kan spelaren spara sitt namn i en highscore-lista.
-        Highscore-datan lagras i SQLite och visas på en server-renderad sida.
+        När spelaren vunnit så kan hen spara sitt namn i en highscore lista genom att fylla i fältet.
+        Highscore-datan lagras i SQLite och visas på en server-side renderad sida.
       </p>
 
       <p>
-        Tekniker som används i projektet är React, Vite, Express, EJS och SQLite.
+        Projektet är skapat med hjälp av React, Vite, Express, EJS och SQLite.
       </p>
     </div>
   );
